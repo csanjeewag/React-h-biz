74474fe8afe7cdb76f6dce80681f547c4df2643d
Chanaka Sanjeewa <Chanakasa@hbs.com>
2021-06-02 00:07:30 +0530

    add route build

---
+++ build/precache-manifest.fa23a04c26027aa76788b921ff8268fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ed3a1f68488fc8cea4cef2e71eef18",
+    "url": "/React-h-biz/index.html"
+  },
+  {
+    "revision": "1b5a8926b888ed9af11c",
+    "url": "/React-h-biz/static/css/main.e32fbe78.chunk.css"
+  },
+  {
+    "revision": "3c36769ec8dad2e4fc70",
+    "url": "/React-h-biz/static/js/2.9ef9b95c.chunk.js"
+  },
+  {
+    "revision": "1b5a8926b888ed9af11c",
+    "url": "/React-h-biz/static/js/main.15ca50bb.chunk.js"
+  },
+  {
+    "revision": "44b2146496d80bfe947f",
+    "url": "/React-h-biz/static/js/runtime-main.ad403ee1.js"
+  }
+]);
